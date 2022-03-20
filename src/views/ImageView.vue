be09fc242aca36cec6d2ec17d07df1b8071d4e72
qi xu <qi.xu@autronicafire.no>
2022-03-20 14:21:06 +0100

    wip

--- src/views/ImageView.vue
+++ src/views/ImageView.vue
@@ -67,56 +67,71 @@
         <div v-html="da"></div>
       </v-row> -->
       <!-- <div>{{ imageData }}</div> -->
-      <div class="image-viewer" v-html="imageData"></div>
+      <v-row align="center" justify="center" class="ml-1">
+        <div class="image-viewer" v-html="imageData"></div>
+      </v-row>
     </div>
     <div v-if="imageInfos.length > 0">
-      <h3 class="ma-4 blue-grey--text">Drawing-Images Info :</h3>
+      <v-row>
+        <h3 class="ma-4 blue-grey--text">Drawing-Images Info :</h3>
+        <v-btn class="ml-2 mt-4" color="#FF5722" small @click="getData(df.id)">
+          <v-icon>mdi-layers-plus</v-icon>
+        </v-btn>
+      </v-row>
       <v-row
-        class="ma-2 ml-6 mb-6"
+        align="center"
+        class="ma-1 ml-6 mb-6"
         v-for="(info, index) in imageInfos"
         :key="index"
       >
         <v-col cols="1">
-          <span class="subheading font-weight-light mr-2 mt-1">ID: </span>
+          <span class="subheading font-weight-light mr-2">ID: </span>
           <span
-            class="subheading font-weight-light mr-4 mt-1 blue--text"
+            class="subheading font-weight-light mr-4 blue--text"
             v-text="info.id"
           ></span>
         </v-col>
         <v-col cols="3">
-          <span class="subheading font-weight-light mr-2 mt-1">Name: </span>
+          <span class="subheading font-weight-light mr-2">Name: </span>
           <span
-            class="subheading font-weight-light mr-4 mt-1 blue--text"
+            class="subheading font-weight-light mr-4 blue--text"
             v-text="info.name"
           ></span>
         </v-col>
         <v-col cols="2">
-          <span class="subheading font-weight-light mr-2 mt-1">Type: </span>
+          <span class="subheading font-weight-light mr-2">Type: </span>
           <span
-            class="subheading font-weight-light mr-4 mt-1 blue--text"
+            class="subheading font-weight-light mr-4 blue--text"
             v-text="info.contentType"
           ></span>
         </v-col>
         <v-col cols="2">
-          <span class="subheading font-weight-light mr-2 mt-1"
-            >Drawing ID:
-          </span>
+          <span class="subheading font-weight-light mr-2">Drawing ID: </span>
           <span
-            class="subheading font-weight-light mr-4 mt-1 blue--text"
+            class="subheading font-weight-light mr-4 blue--text"
             v-text="info.drawingId"
           ></span>
         </v-col>
         <v-col cols="2">
-          <span class="subheading font-weight-light mr-2 mt-1">Layer: </span>
+          <span class="subheading font-weight-light mr-2">Layer: </span>
           <span
-            class="subheading font-weight-light mr-4 mt-1 blue--text"
+            class="subheading font-weight-light mr-4blue--text"
             v-text="info.subLayer"
           ></span>
         </v-col>
-        <v-col cols="2">
-          <v-btn color="primary" small @click="getImage(info.id)">
+        <v-col cols="1">
+          <v-btn color="#FFAB00" small @click="getImage(info.id)">
             <v-icon>mdi-eye</v-icon>
           </v-btn>
+        </v-col>
+        <v-col cols="1">
+          <v-checkbox
+            class="mt-0"
+            color="secondary"
+            hide-details
+            :value="checkIncludes(info.id)"
+            @change="setSelectedSubImages(info.id, $event)"
+          ></v-checkbox>
         </v-col>
       </v-row>
     </div>
@@ -151,6 +166,7 @@ export default Vue.extend({
     imageDatas: string[];
     imageData: string;
     currentDrawingData: any;
+    selectedImageIDs: number[];
   } {
     return {
       drawingFiles: [],
@@ -158,12 +174,16 @@ export default Vue.extend({
       imageDatas: [],
       imageData: '',
       currentDrawingData: null,
+      selectedImageIDs: [],
     };
   },
 
   methods: {
     listDrawings() {
       this.drawingFiles = [];
+      this.imageData = '';
+      this.currentDrawingData = null;
+      this.selectedImageIDs = [];
       listUploadedDrawings()
         .then((response) => this.setDrawings(response))
         .catch((error) => {
@@ -174,6 +194,7 @@ export default Vue.extend({
       this.imageDatas = [];
       this.imageData = '';
       this.currentDrawingData = null;
+      this.selectedImageIDs = [];
       fetchImagesInfo(id, 'svg', layer)
         .then((response) => this.setImages(response))
         .catch((error) => {
@@ -217,12 +238,30 @@ export default Vue.extend({
       this.imageData = data;
     },
 
+    setSelectedSubImages(id: number, selected: boolean) {
+      let index = this.selectedImageIDs.indexOf(id);
+      if (index === -1) {
+        if (selected) {
+          this.selectedImageIDs.push(id);
+        }
+      } else {
+        if (!selected) {
+          this.selectedImageIDs.splice(index, 1);
+        }
+      }
+    },
+
+    checkIncludes(id: number): boolean {
+      return this.selectedImageIDs.includes(id);
+    },
+
     clearSelection() {
       this.imageData = '';
       this.imageInfos = [];
       this.drawingFiles = [];
       this.imageDatas = [];
       this.currentDrawingData = null;
+      this.selectedImageIDs = [];
     },
   },
 });
@@ -242,9 +281,11 @@ export default Vue.extend({
 }
 
 .image-viewer {
-  border: 4px dotted blue;
   display: flex;
-  width: 600px;
-  height: 600px;
+  border: 3px dotted rgb(147, 197, 243);
+  display: flex;
+  width: 500px;
+  height: 500px;
+  align-items: center;
 }
 </style>
